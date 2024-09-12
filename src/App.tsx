a364bdb857de7c42b925bf125859a19be46b57d1
azabiyaliev <aza.biyaliev@gmail.com>
2024-09-12 18:12:57 +0600

    Навесил событие на кнопку, а также в ней создал пустой массив, после генерировал рандомные числа, указал диапозон от 5 до 36, фильтровал уникальные числа, добавлял их в пустой массив, новый массив перезаписывал через setNumbers.

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,43 @@ import {useState} from 'react';
 
 const App = () => {
 
-  const [numbers] = useState<number[]>([5, 11, 16, 23, 32]);
+  const [numbers, setNumbers] = useState<number[]>([5, 11, 16, 23, 32]);
 
   const numbersDraw = numbers.map(number => {
-    console.log(number);
     return (
         <Numbers number={number}/>
     );
   });
 
+  const newNumbersBtn = () => {
+
+      const numbersArray = [];
+
+      const getRandomInt = (min:number, max:number) => {
+          min = Math.ceil(min);
+          max = Math.floor(max);
+          return Math.floor(Math.random() * (max - min + 1)) + min;
+      };
+
+      for (let i = 1; i < 15; i++) {
+          const randomInt = getRandomInt(5, 36);
+          numbersArray.push(randomInt);
+      }
+
+      const uniqueNumber = new Set(numbersArray);
+      const uniqueArray = Array.from(uniqueNumber);
+      const limitArray: number[] = uniqueArray.slice(-5);
+      const newArray:number[] = [];
+      for (let i = 0; i < limitArray.length; i++) {
+          limitArray.sort((a, b) => a - b);
+          newArray.push(limitArray[i]);
+      }
+        setNumbers(newArray);
+  };
+
   return (
       <>
-          <button type="button" className="btn">New numbers</button>
+          <button onClick={newNumbersBtn} type="button" className="btn">New numbers</button>
           <div className="numbers">
               {numbersDraw}
           </div>
